7d82f7fea6f906aeb419a2855d03b0413775bb67
noshiro-pf <noshiro.pf@gmail.com>
2025-06-17 08:05:08 +0900

    chore: update scripts

--- configs/vitest.config.ts
+++ configs/vitest.config.ts
@@ -1,6 +1,6 @@
 import * as path from 'node:path';
 import { defineConfig } from 'vitest/config';
-import { projectRootPath } from '../scripts/utils.mjs';
+import { projectRootPath } from '../scripts/project-root-path.mjs';
 
 // https://github.com/vitest-dev/vitest/blob/v1.5.0/test/import-meta/vite.config.ts
 export default defineConfig({
@@ -18,6 +18,7 @@ export default defineConfig({
       provider: 'v8',
       reporter: ['html', 'lcov', 'text'],
       include: ['src/**'],
+      exclude: ['**/index.mts'],
     },
   },
 });
